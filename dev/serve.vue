52ce0df0caa4707c589e293145d06a31df79d9ac
Manas Paul <manas@codelogicx.com>
2021-05-04 12:14:13 +0530

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VLinkify from '@/v-linkify.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VLinkify
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <v-linkify />
+  </div>
+</template>
---
+++ src/v-linkify.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-html="foramttedText"></div>
+  </div>
+</template>
+
+<script>
+import linkifyHtml from 'linkifyjs/html'
+
+export default /*#__PURE__*/{
+  name: 'VLinkify', // vue component name
+  props: {
+    html: {
+      type: Text,
+      required: true,
+      default: `Your text will goes here..`
+    }
+  },
+  computed: {
+    foramttedText() {
+      return this.convertToLink(this.html)
+    }
+  },
+  methods: {
+    // convert to linkify
+    convertToLink(data) {
+      const markup = data
+        .replace(/</g, '&lt;')
+        .replace(/((?:href|src)=['"])(.*?)(['"])/g, (matched, prefix, href, suffix) => {
+          return `${prefix}<a href="${href}" target="_blank">${href}</a>${suffix}`
+        })
+
+      return linkifyHtml(markup, { target: { url: "_blank" } })
+    }
+  }
+};
+</script>
